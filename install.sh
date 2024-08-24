912f59e8f5371583492221945c438b5b4288f877
Patrick <96850073+PATRICK297R@users.noreply.github.com>
2024-08-24 07:05:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,6 @@ instalartema(){
     cd pterodactylthemes
     rm /var/www/pterodactyl/resources/scripts/pterodactylthemes.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
-    rm /var/www/pterodactyl/resources/scripts/components/server/files/FileManagerContainer.tsx
-    mv FileManagerContainer.tsx /var/www/pterodactyl/resources/scripts/components/server/files/FileManagerContainer.tsx
-    mv FetchButton.tsx /var/www/pterodactyl/resources/scripts/components/server/files/FetchButton.tsx
     mv index.tsx /var/www/pterodactyl/resources/scripts/index.tsx
     mv pterodactylthemes.css /var/www/pterodactyl/resources/scripts/pterodactylthemes.css
     cd /var/www/pterodactyl
