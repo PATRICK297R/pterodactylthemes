cfc38fea46e1decfd10c019916bd815e93d175a7
Patrick <96850073+PATRICK297R@users.noreply.github.com>
2024-08-24 07:12:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ restaurarbackup(){
 
     CYAN='\033[0;36m'
     echo -e "${CYAN}Copyright 2023 Patrick Theme"
-    echo -e "${CYAN}This program is free software, you can modify and distribute it without any problems."
+    echo -e "${CYAN}This program is free software."
     echo -e ""
     echo -e "${CYAN}Website : https://nextsekai.in/"
     echo -e ""
