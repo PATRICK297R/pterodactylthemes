92708bf2506c610e0e9d90fda26971ef6e429eff
Patrick <96850073+PATRICK297R@users.noreply.github.com>
2024-08-24 07:01:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ instalartema(){
     echo -e "${CYAN}Installing themes..."
     cd /var/www/pterodactyl
     rm -r pterodactylthemes
-    git clone https://github.com/BOTCAHX/pterodactylthemes.git
+    git clone https://github.com/PATRICK297R/pterodactylthemes.git
     cd pterodactylthemes
     rm /var/www/pterodactyl/resources/scripts/pterodactylthemes.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
